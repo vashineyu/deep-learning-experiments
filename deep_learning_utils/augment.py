cb84bb76e2a3a6650477d13caf4e617bb5416482
vashineyu <vashineyu@gmail.com>
2018-07-05 18:09:16 +0800

    add customized aug function example

---
+++ deep_learning_utils/augment.py
@@ -0,0 +1,43 @@
+import imgaug as ia
+from imgaug import augmenters as iaa
+
+# Customize iaa function
+def image_channel_suffle(image):
+    img_ = image.copy()
+    #r, g, b = img_[:,:,0], img_[:,:,1], img_[:,:,2]
+    if img_.shape[-1] == 1:
+        raise ValueError("Input should be RGB")
+    idx = [0,1,2]
+    np.random.shuffle(idx)
+    #img_[:,:,idx[0]], img_[:,:,idx[1]], img_[:,:,idx[2]] = r, g, b 
+    return img_[:,:,idx]
+
+def img_func(images, random_state, parents, hooks):
+    for img in images:
+        img = image_channel_suffle(img)
+    return images
+
+def keypoint_func(keypoints_on_images, random_state, parents, hooks):
+    return keypoints_on_images
+
+
+
+### Common Usage Setting ###
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+seq = iaa.Sequential([
+    iaa.Fliplr(0.5, name="FlipLR"),
+    iaa.Flipud(0.5, name="FlipUD"),
+    iaa.Multiply((0.8, 1.2), name = "Multiply"),
+    iaa.ContrastNormalization((0.8, 1.1), name = "Contrast"),
+    iaa.AddToHueAndSaturation((-30,30),name = "Hue"),
+    iaa.OneOf([iaa.AdditiveGaussianNoise(scale=0.02*255, name="Noise"),
+               iaa.CoarseDropout((0.01, 0.05), size_percent=(0.01, 0.025), name = 'Cdrop')
+               ]),
+    iaa.Affine(scale = (0.75,1.25), cval=0, mode = 'constant'), # wrap
+    iaa.OneOf([iaa.Affine(rotate = 90),
+               iaa.Affine(rotate = 180),
+               iaa.Affine(rotate = 270)]),
+    sometimes(iaa.GaussianBlur((0, 1.5), name="GaussianBlur")),
+    iaa.Lambda(img_func, keypoint_func, name = 'channel_swap'),
+    sometimes(iaa.PerspectiveTransform(scale=(0.025, 0.100)))
+])
