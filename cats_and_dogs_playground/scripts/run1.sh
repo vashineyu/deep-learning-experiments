22d814f2a7dd72cd8dc23d6a3f859819e487e603
seanyu <r01227113@g.ntu.edu.tw>
2019-04-24 14:27:46 +0800

    first trim unrelated stuff

--- cats_and_dogs_playground/scripts/run1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python run.py --config-file configs/R50v2_GNv2_BZ1_noPretrain.yaml SYSTEM.DEVICES [6]
-python run.py --config-file configs/R50v2_GNv2_BZ1_Pretrain.yaml SYSTEM.DEVICES [6]
-
-
